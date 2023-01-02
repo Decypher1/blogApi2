7c66f07491bc66714ef979efe583bd5a9436cdb2
decypher1 <mumekwe2@gmail.com>
2023-01-02 20:54:57 +0100

    Database folder

---
+++ db/mongodb.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const config = require("../config/config")
+
+function dbConnect(){
+    mongoose.connect(config.MONGODB_URL)
+
+    mongoose.connection.on("connected", () => {
+        console.log('Mongodb connected successfully')
+    })
+
+    mongoose.connection.on("error", (err) => {
+        console.log('An error Occured, NOT connected')
+        console.log(err)
+    })
+}
+
+module.exports = dbConnect()
